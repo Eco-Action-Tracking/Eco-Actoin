5d0a4d002574f3879c0deb6949e659950a5e9aa0
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-10-10 09:37:32 +0300

    Models

---
+++ my-app/src/models/productsModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  category: { type: String },
+  picture: { type: [String] },
+  isDeleted: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
